7de4b338b913a35f78f4dedf9b6bd291dc25d212
unknown <you@example.com>
2023-04-09 10:56:32 +0700

    first commit

---
+++ Controllers/MakeFormController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Makeform.Models;
+using System.IO;
+namespace Makeform.Controllers
+{
+    public class MakeFormController : Controller
+    {
+        // GET: MakeForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult KetQua()
+        {
+            return View(SinhVien.dssinhvien);
+        }
+        [HttpPost]
+        public ActionResult XuLy(SinhVien sv,HttpPostedFileBase filAnh)
+        {
+            if (ModelState.IsValid)
+            {
+              if (filAnh.ContentLength > 0 && filAnh != null )
+            {
+                //luu file 
+                string hinhanh = Server.MapPath("/Data/");
+                string duongdan = hinhanh + filAnh.FileName;
+                filAnh.SaveAs(duongdan);
+                sv.Anh = "/Data/" + filAnh.FileName;
+            }     
+                
+            SinhVien.dssinhvien.Add(sv);return RedirectToAction("KetQua");
+            }
+           else
+            {
+                return RedirectToAction("XuLy");
+            }
+            
+            
+            
+            
+
+
+
+            
+            
+        }
+        
+    }
+}
---
+++ Models/SinhVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Makeform.Models
+{
+    public class SinhVien
+    {
+        public static List<SinhVien> dssinhvien = new List<SinhVien>();
+        [Required(ErrorMessage ="Khong dc de trong")]
+        
+       
+        public string MaSV { get; set; }
+
+        public string TenSV { get; set; }
+        public string NgaySinh { get; set; }
+        public float Toan { get; set; }
+        public float Ly { get; set; }
+        public float Hoa { get; set; }
+        public double tongdiem
+        {
+            get
+            {
+                return Toan + Ly + Hoa;
+            }
+        }
+        public string Anh { get; set; }
+
+
+
+
+
+
+
+    }
+}
